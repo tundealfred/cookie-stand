e827db28774d120fadf5fba30331fe91d730cfb3
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-09-27 13:02:46 +0100

    update table and css

--- app.js
+++ app.js
@@ -38,29 +38,50 @@ const hours = [
   "7pm",
 ];
 
+// get the table from the HTML so we can add rows
+const table = document.getElementById("displaySalesData");
+
 CookieShops.prototype.render = function () {
   this.calculateSales();
 
-  //seattle.calculateSales();
-  //tokyo.calculateSales();
+  const tr = document.createElement("tr");
 
-  // Display the data for Seattle
-  const displaySalesData = document.getElementById("displaySalesData");
-  const article = document.createElement("article");
+  // add the store name to the row
+  const th = document.createElement("th");
+  th.textContent = this.location;
+  tr.appendChild(th);
 
-  const h2 = document.createElement("h2");
-  h2.textContent = this.location;
-  article.appendChild(h2);
-
-  const ul = document.createElement("ul");
-  for (let i = 0; i < hours.length; i++) {
-    const li = document.createElement("li");
-    li.textContent = `${hours[i]}  :  ${this.cookiesPerHour[i]} Cookies`;
-    ul.appendChild(li);
+  // add this store's data to that row
+  for (let i = 0; i < this.cookiesPerHour.length; i++) {
+    // create a new td and put the sales number in it
+    const td = document.createElement("td");
+    td.textContent = this.cookiesPerHour[i];
+    tr.appendChild(td);
   }
-  article.appendChild(ul);
 
-  displaySalesData.appendChild(article);
+  // add that row to the table
+  table.appendChild(tr);
+
+  // //seattle.calculateSales();
+  // //tokyo.calculateSales();
+
+  // // Display the data for Seattle
+  // const displaySalesData = document.getElementById("displaySalesData");
+  // const article = document.createElement("article");
+
+  // const h2 = document.createElement("h2");
+  // h2.textContent = this.location;
+  // article.appendChild(h2);
+
+  // const ul = document.createElement("ul");
+  // for (let i = 0; i < hours.length; i++) {
+  //   const li = document.createElement("li");
+  //   li.textContent = `${hours[i]}  :  ${this.cookiesPerHour[i]} Cookies`;
+  //   ul.appendChild(li);
+  // }
+  // article.appendChild(ul);
+
+  // displaySalesData.appendChild(article);
 };
 
 // Create instances for each shop
